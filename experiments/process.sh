47a921a3195c1a27b11cae8dcde364d65aac12cf
Panagiotis <el15066@central.ntua.gr>
2022-02-12 14:32:17 +0200

    Update process.sh

--- experiments/process.sh
+++ experiments/process.sh
@@ -1,12 +1,15 @@
-#!/bin/bash -vxe
+#!/bin/bash -vx
 
-RUN="$1"
+for RUN in $@; do (
 
-[ "$RUN" ] || exit 1
+set -e
 
 ~/el15066/Diplomatiki/tools/parse_log.py     "$RUN/log.txt"    "$RUN/log.csv" >"$RUN/parse_log.txt"
 ~/git/FlameGraph/stackcollapse-perf.pl       "$RUN/perf.txt"                  >"$RUN/folded.txt"
 ~/el15066/Diplomatiki/tools/stack_top.py     "$RUN/folded.txt"                >"$RUN/stack_top.txt"
 ~/el15066/Diplomatiki/tools/stack_uncycle.py "$RUN/folded.txt"                >"$RUN/stack_uncycle.txt"
 
-chmod -w -R "$RUN/"
+chmod  -w -R "$RUN/"
+chmod u+w    "$RUN/README.md"
+
+) done
