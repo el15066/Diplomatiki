c2bd768407c36976a4a46a0bec2f115520462147
Panagiotis <el15066@central.ntua.gr>
2022-02-12 20:16:24 +0200

    stack_top_uncycle

--- experiments/process.sh
+++ experiments/process.sh
@@ -2,10 +2,11 @@
 
 for RUN in $@; do
 
-~/el15066/Diplomatiki/tools/parse_log.py     "$RUN/log.txt"    "$RUN/log.csv" >"$RUN/parse_log.txt" &&
-~/git/FlameGraph/stackcollapse-perf.pl       "$RUN/perf.txt"                  >"$RUN/folded.txt" &&
-~/el15066/Diplomatiki/tools/stack_top.py     "$RUN/folded.txt"                >"$RUN/stack_top.txt" &&
-~/el15066/Diplomatiki/tools/stack_uncycle.py "$RUN/folded.txt"                >"$RUN/stack_uncycle.txt" &&
+~/el15066/Diplomatiki/tools/parse_log.py     "$RUN/log.txt"    "$RUN/log.csv" >"$RUN/parse_log.txt"
+~/git/FlameGraph/stackcollapse-perf.pl       "$RUN/perf.txt"                  >"$RUN/folded.txt"
+~/el15066/Diplomatiki/tools/stack_top.py     "$RUN/folded.txt"                >"$RUN/stack_top.txt"
+~/el15066/Diplomatiki/tools/stack_uncycle.py "$RUN/folded.txt"                >"$RUN/stack_uncycle.txt"
+~/el15066/Diplomatiki/tools/stack_uncycle.py "$RUN/stack_top.txt"             >"$RUN/stack_top_uncycle.txt" &&
 
 chmod  -w -R "$RUN/" &&
 chmod u+w    "$RUN/" "$RUN/README.md"
